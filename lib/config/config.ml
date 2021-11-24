4722b1610c10f2e6df5e1c2f26de4b61ea3abf34
Anurag Soni <anurag@sonianurag.com>
2021-11-23 22:13:14 -0500

    fix config.h generator

--- lib/config/config.ml
+++ lib/config/config.ml
@@ -2,8 +2,8 @@ module C = Configurator.V1
 module SMap = Map.Make (String)
 
 let kqueue_available vars =
-  SMap.exists
-    (fun _ v ->
+  List.exists
+    (fun (_, v) ->
       match v with
       | C.C_define.Value.Switch true -> true
       | _ -> false)
@@ -29,16 +29,13 @@ let () =
           ; "__NetBSD__", C.C_define.Type.Switch
           ]
       in
-      let results =
-        SMap.of_seq (List.to_seq (List.concat [ operating_systems; system ]))
-      in
       let vars =
-        [ "KQUEUE_AVAILABLE", C.C_define.Value.Switch (kqueue_available results)
-        ; "FREEBSD", SMap.find "__FreeBSD__" results
-        ; "OPENBSD", SMap.find "__OpenBSD__" results
-        ; "DRAGONFLY", SMap.find "__DragonFly__" results
-        ; "NETBSD", SMap.find "__NetBSD__" results
-        ; "KQUEUE_ML_ARCH_SIXTYFOUR", SMap.find "ARCH_SIXTYFOUR" results
+        [ "KQUEUE_AVAILABLE", C.C_define.Value.Switch (kqueue_available operating_systems)
+        ; "FREEBSD", List.assoc "__FreeBSD__" operating_systems
+        ; "OPENBSD", List.assoc "__OpenBSD__" operating_systems
+        ; "DRAGONFLY", List.assoc "__DragonFly__" operating_systems
+        ; "NETBSD", List.assoc "__NetBSD__" operating_systems
+        ; "KQUEUE_ML_ARCH_SIXTYFOUR", List.assoc "ARCH_SIXTYFOUR" system
         ]
       in
       C.C_define.gen_header_file conf ~fname:"config.h" vars)
